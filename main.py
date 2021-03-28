432e3f410537e2e7a13d2fb6ee9304655698cda6
Porush Puri <porushpuri1@gmail.com>
2021-03-28 21:31:52 +0530

    Add Basic Setup for Streamlit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import streamlit as st
 
+data = pd.read_csv('Debt_GDP_World.csv')
+
+st.title("Comparing GDP to Debt Ratio of Different Countries!")
+st.subheader("We will be comparing diiferent countrie's GDP to Debt Ratio to see if we can mind any comparitive similarities or disparities in Policy Making which makes them different.")
+
+
+
+
